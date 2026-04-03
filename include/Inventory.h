698387659cc5942cedf24afa89ccb3d21dad15e6
ZeroDay-del <224141417@stu.ukzn.ac.za>
2026-04-03 02:02:02 +0200

    Add Inventory class

---
+++ include/Inventory.h
@@ -0,0 +1,16 @@
+#ifndef INVENTORY_H
+#define INVENTORY_H
+
+
+class Inventory
+{
+    public:
+        Inventory();
+        virtual ~Inventory();
+
+    protected:
+
+    private:
+};
+
+#endif // INVENTORY_H
