cc3593f4a741fd06b62da99472b971f6b1a6bd54
ZeroDay-del <224141417@stu.ukzn.ac.za>
2026-04-03 02:14:52 +0200

    Finish Inverntory Class

--- include/Inventory.h
+++ include/Inventory.h
@@ -1,16 +1,22 @@
 #ifndef INVENTORY_H
 #define INVENTORY_H
-
+#include "product.h"
 
 class Inventory
 {
     public:
         Inventory();
         virtual ~Inventory();
+        bool addProduct(Product &product) const;
+        int getSize() const;
+        int getCapacity() const;
 
     protected:
 
     private:
+        Product *prod;
+        int sizeI;
+        int capacity;
 };
 
 #endif // INVENTORY_H
--- src/Inventory.cpp
+++ src/Inventory.cpp
@@ -2,10 +2,24 @@
 
 Inventory::Inventory()
 {
-    //ctor
+    sizeI = 0;
+    capacity = 3;
 }
 
 Inventory::~Inventory()
 {
     //dtor
+}
+
+int Inventory::getSize() const{
+    return this->sizeI;
+}
+
+int Inventory::getCapacity() const{
+    return this->capacity;
+}
+
+bool Inventory::addProduct(Product &product)const{
+    // The logic is here
+    return true;
 }
